fa7f59ae7714679c37931ac4346d04d644cb9872
Simon McDuff <smcduff@hotmail.com>
2008-06-13 22:35:38 +0000

    233490: Change Subscription https://bugs.eclipse.org/bugs/show_bug.cgi?id=233490

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/CDONotification.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/CDONotification.java
@@ -19,8 +19,19 @@ import org.eclipse.emf.common.notify.Notification;
  */
 public interface CDONotification extends Notification
 { 
+  /**
+   * Notification often have a list of notification (NotificationChain). 
+   * It informs the adapter if another another notification will be sent. 
+   * <p>
+   * You don`t want to refresh the UI for every Notifications. 
+   * The same object could receives many notifications at the same time. To know when to perform your 
+   * update you could use that value. It will be the last notification when it will return false.
+   */
   boolean hasNext();
   
+  /**
+   * Return the {@link CDORevisionDelta} associate to this Notification. 
+   */
   public CDORevisionDelta getRevisionDelta();
  
 }
