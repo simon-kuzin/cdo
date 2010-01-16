ae54c58eb69054a0137f04951f08076456087d7c
Eike Stepper <stepper@esc-net.de>
2010-01-16 16:06:22 +0000

    [270716]  Provide support for branching https://bugs.eclipse.org/bugs/show_bug.cgi?id=270716

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/BranchingTest.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/BranchingTest.java
@@ -230,8 +230,10 @@ public class BranchingTest extends AbstractCDOTest
 
     check(session, mainBranch, commitTime1, 5, "CDO");
     check(session, mainBranch, commitTime2, 5, "CDO");
+    check(session, mainBranch, CDOBranchPoint.UNSPECIFIED_DATE, 5, "CDO");
     check(session, subBranch, commitTime1, 5, "CDO");
     check(session, subBranch, commitTime2, 10, "CDO");
+    check(session, subBranch, CDOBranchPoint.UNSPECIFIED_DATE, 10, "CDO");
     session.close();
   }
 
