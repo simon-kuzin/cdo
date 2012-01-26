06caef367e263da4cc302a6307841a7e95f0e2bc
Stefan Winkler <stefan@winklerweb.net>
2012-01-26 13:17:38 +0100

    Added another missing if(TRACER.isEnabled()) - copy and paste error from before.

--- plugins/org.eclipse.emf.cdo.server.db/src/org/eclipse/emf/cdo/server/internal/db/mapping/horizontal/BranchingListTableMappingWithRanges.java
+++ plugins/org.eclipse.emf.cdo.server.db/src/org/eclipse/emf/cdo/server/internal/db/mapping/horizontal/BranchingListTableMappingWithRanges.java
@@ -1032,6 +1032,7 @@ public class BranchingListTableMappingWithRanges extends BasicAbstractListTableM
             else
             {
               value = getValueFromBase(accessor, id, branchId, index);
+              if (TRACER.isEnabled())
               {
                 TRACER.format("moveOneDown add historic entry at: {0}", index); //$NON-NLS-1$
               }
